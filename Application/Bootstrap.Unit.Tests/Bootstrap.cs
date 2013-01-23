ae68e7b6c994a29f8a0999bbb0a03bb672c35563
Peter Bartels <peterbartels@gmail.com>
2013-01-23 12:56:20 +0000

    Added tests to see if teamview is going to work

---
+++ Application/Bootstrap.Unit.Tests/Bootstrap.cs
@@ -0,0 +1,20 @@
+﻿using NUnit.Framework;
+using FluentAssertions;
+
+namespace Bootstrap.Unit.Tests
+{
+    public class Bootstrap
+    {
+        [Test]
+        public void ShouldBeAbleToInitialize()
+        {
+            "nothing".Should().Be("nothing");
+        }
+
+        [Test]
+        public void ShouldBeAbleToFail()
+        {
+            "Something".Should().Be("nothing");
+        }
+    }
+}
