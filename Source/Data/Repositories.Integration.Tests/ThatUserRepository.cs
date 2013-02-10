1dae23ec641b691dd755a34e0a85c86b6f410a57
Peter Bartels <peterbartels@gmail.com>
2013-02-10 10:13:00 +0000

    Moved all the projects to a source folder and created a build folder

---
+++ Source/Data/Repositories.Integration.Tests/ThatUserRepository.cs
@@ -0,0 +1,68 @@
+﻿using System.Linq;
+using FluentAssertions;
+using Informedica.GenPres.Business.Entities;
+using NUnit.Framework;
+using Raven.Client;
+using Raven.Client.Embedded;
+using Raven.Database.Server;
+using Shared.Test;
+
+namespace Repositories.Integration.Tests
+{
+    [TestFixture]
+    public class ThatUserRepository
+    {
+        private const string Username = "admin";
+        private const string PasswordHash = "590489053890";
+        
+        private IDocumentSession _session;
+        private EmbeddableDocumentStore _documentStore;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _documentStore = new EmbeddableDocumentStore
+            {
+                RunInMemory = true
+            };
+
+            _documentStore.Configuration.AnonymousUserAccessMode = AnonymousUserAccessMode.All;
+            _documentStore.Initialize();
+            _session = _documentStore.OpenSession();
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            _session.Dispose();
+            _documentStore.Dispose();
+        }
+
+        [Test]
+        public void ShouldBeAbleToCreateANewUser()
+        {
+            var userRepository = new UserRepository(_session);
+            userRepository.CreateUser(Username, PasswordHash);
+            _session.SaveChanges();
+
+            var getUser = _session.Query<User>().Single(user => user.Username == Username);
+            getUser.Username.Should().NotBeEmpty();
+        }
+
+        [Test]
+        public void ShouldBeAbleToRetrieveAUser()
+        {
+            var user = User.CreateUser(Username, PasswordHash);
+            _session.Store(user);
+            _session.SaveChanges();
+
+            var userRepository = new UserRepository(_session);
+            var getUser = userRepository.GetUser(Username);
+            
+            AssertAll.Succeed(
+                () => Assert.IsNotNullOrEmpty(getUser.PasswordHash, "password should be retrieved"),
+                () => Assert.IsNotNullOrEmpty(getUser.Username, "username should have been retrieved")
+            );
+        }
+    }
+}
---
+++ Source/Data/Repositories/UserRepository.cs
@@ -0,0 +1,27 @@
+﻿using System.Linq;
+using Informedica.GenPres.Business.Entities;
+using Raven.Client;
+
+namespace Repositories
+{
+    public class UserRepository : RavenDbRepository<User>, IUserRepository
+    {
+        public UserRepository(IDocumentSession session)
+            : base(session)
+        {
+            
+        }
+
+        public User CreateUser(string username, string password)
+        {
+            var user = User.CreateUser(username, password);
+            Session.Store(user);
+            return user;
+        }
+
+        public User GetUser(string username)
+        {
+            return Session.Query<User>().Single(user => user.Username == username);
+        }
+    }
+}
