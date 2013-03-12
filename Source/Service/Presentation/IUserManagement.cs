472c05e1ea8a9edf60c5859ce67a158f624f0d37
Peter Bartels <peterbartels@gmail.com>
2013-03-12 13:06:29 +0000

    Changed interface for Usermanagement

--- Source/Service/Presentation/IUserManagement.cs
+++ Source/Service/Presentation/IUserManagement.cs
@@ -1,6 +1,11 @@
-﻿namespace Informedica.Service.Presentation
+﻿using System.Collections.Generic;
+
+namespace Informedica.Service.Presentation
 {
     public interface IUserManagement
     {
+        List<UserDto> GetUsers();
+        void AddUser(UserDto userDto);
+        void DeleteUser(UserDto userDto);
     }
 }
